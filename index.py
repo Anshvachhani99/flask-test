6a9760b9167f684b428d37e4e586a9b4e65a3808
Ansh vachhani <92299607+Anshvachhani99@users.noreply.github.com>
2024-04-07 12:12:39 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def start():
-    return "The MBSA Server is Running"
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-@app.route("/mbsa")
-def mbsa():
-    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
